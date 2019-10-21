d798d3d41eeaf19391c4eaa709e95200e7f463b0
derekho1996 <derekho1996@gmail.com>
2019-10-21 19:01:36 +0800

    Add files via upload

---
+++ LogIn_4.2/LogIn_4.2/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace LogIn_4._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtApp_ID = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtApp_Pwd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnApprove = new System.Windows.Forms.Button();
+            this.loginBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appData = new LogIn_4._2.AppData();
+            this.loginTableAdapter = new LogIn_4._2.AppDataTableAdapters.LoginTableAdapter();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.dataGridView);
+            this.groupBox.Controls.Add(this.btnEdit);
+            this.groupBox.Controls.Add(this.label5);
+            this.groupBox.Controls.Add(this.txtApp_ID);
+            this.groupBox.Controls.Add(this.btnDelete);
+            this.groupBox.Controls.Add(this.txtApp_Pwd);
+            this.groupBox.Controls.Add(this.label4);
+            this.groupBox.Controls.Add(this.btnApprove);
+            this.groupBox.Location = new System.Drawing.Point(169, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(467, 380);
+            this.groupBox.TabIndex = 9;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "REGISTRATION";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView.GridColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dataGridView.Location = new System.Drawing.Point(8, 36);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(449, 221);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.NullValue = false;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column1.HeaderText = "Select";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 43;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column3.HeaderText = "Employee ID";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Access level";
+            this.Column4.Items.AddRange(new object[] {
+            "Operator",
+            "Technician",
+            "Engineer"});
+            this.Column4.Name = "Column4";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(371, 336);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Password";
+            // 
+            // txtApp_ID
+            // 
+            this.txtApp_ID.Location = new System.Drawing.Point(136, 294);
+            this.txtApp_ID.Name = "txtApp_ID";
+            this.txtApp_ID.Size = new System.Drawing.Size(198, 20);
+            this.txtApp_ID.TabIndex = 5;
+            this.txtApp_ID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtApp_ID_KeyPress);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(371, 304);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // txtApp_Pwd
+            // 
+            this.txtApp_Pwd.Location = new System.Drawing.Point(136, 320);
+            this.txtApp_Pwd.Name = "txtApp_Pwd";
+            this.txtApp_Pwd.Size = new System.Drawing.Size(198, 20);
+            this.txtApp_Pwd.TabIndex = 5;
+            this.txtApp_Pwd.UseSystemPasswordChar = true;
+            this.txtApp_Pwd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtApp_Pwd_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Approve Employee ID";
+            // 
+            // btnApprove
+            // 
+            this.btnApprove.Location = new System.Drawing.Point(371, 272);
+            this.btnApprove.Name = "btnApprove";
+            this.btnApprove.Size = new System.Drawing.Size(75, 23);
+            this.btnApprove.TabIndex = 7;
+            this.btnApprove.Text = "APPROVE";
+            this.btnApprove.UseVisualStyleBackColor = true;
+            this.btnApprove.Click += new System.EventHandler(this.BtnApprove_Click);
+            // 
+            // loginBindingSource
+            // 
+            this.loginBindingSource.DataMember = "Login";
+            this.loginBindingSource.DataSource = this.appData;
+            // 
+            // appData
+            // 
+            this.appData.DataSetName = "AppData";
+            this.appData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // loginTableAdapter
+            // 
+            this.loginTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 447);
+            this.Controls.Add(this.groupBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LogIn_4.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column4;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtApp_ID;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtApp_Pwd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnApprove;
+        private AppData appData;
+        private System.Windows.Forms.BindingSource loginBindingSource;
+        private AppDataTableAdapters.LoginTableAdapter loginTableAdapter;
+    }
+}
+
---
+++ LogIn_4.2/LogIn_4.2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace LogIn_4._2
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\derek.ho\Documents\UserDatabase.mdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'appData.Login' table. You can move, or remove it, as needed.
+            this.loginTableAdapter.Fill(this.appData.Login);
+            dataGridView.Rows.Add(9);           //Declare empty row for the table
+            Display(true);                     //Disable all button
+            for (int i = 1; i < 9; i++)         //Display the 1st row only
+            {
+                dataGridView.Rows[i].ReadOnly = false;
+                dataGridView.Rows[i].Visible  = true;
+            }
+            txtApp_ID.Select();
+        }
+
+        private void Display(object sender)     //Function to enable and disable the button and textbox
+        {
+            
+            if (sender.Equals(false))
+            {
+                txtApp_ID.Clear(); txtApp_Pwd.Clear();
+                btnApprove.Enabled = false;
+                btnDelete.Enabled = false;
+                btnEdit.Enabled = false;
+                txtApp_ID.Focus();
+            }
+            else if (sender.Equals(true))
+            {
+                btnApprove.Enabled = true;
+                btnDelete.Enabled = true;
+                btnEdit.Enabled = true;
+            }
+        }
+
+        private void TxtApp_ID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                txtApp_Pwd.Focus();
+        }
+
+        private void TxtApp_Pwd_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if ((String.IsNullOrEmpty(txtApp_ID.Text)) || (String.IsNullOrEmpty(txtApp_Pwd.Text)))
+                {
+                    MessageBox.Show("Invalid Login", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Display(false);
+                    return;
+                }
+                try
+                {
+                    AppDataTableAdapters.LoginTableAdapter login = new AppDataTableAdapters.LoginTableAdapter();
+                    AppData.LoginDataTable dt = login.GetDataByIDPassword(txtApp_ID.Text, txtApp_Pwd.Text);
+                    if(dt.Rows.Count == 1)
+                    {
+                        MessageBox.Show("Vaild login", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Display(true);
+                        txtApp_ID.Enabled = false;
+                        txtApp_Pwd.Enabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Login", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Display(false);
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnApprove_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                if ((Convert.ToBoolean(row.Cells[0].Value) == true))        //check for the tick box is selected
+                {
+                    cmd.CommandText = "insert into Login (EmployeeID,Name) values ('" + row.Cells[1] + "','" + row.Cells[2] + "')";
+                    //,EmployeeID,Access,Permission
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            con.Close();
+            MessageBox.Show("User registered successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+
+
+    }
+}
+/*
+foreach (DataGridViewCell cell in row.Cells)            //check for each cell to make sure is not null
+                    {
+                        if (cell.Value != null)
+                        {
+                            Valid_count++;
+                        }
+                        else
+                        {
+                            MessageBox.Show("NULL!!");
+                        }
+                    }
+*/
